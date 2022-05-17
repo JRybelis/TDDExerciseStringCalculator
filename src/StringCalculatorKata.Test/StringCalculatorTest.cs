97156700f4a00b8e6aef56ced184a79a9defcfc8
jokubasr <jokubas.rybelis@softra.lt>
2022-05-17 10:05:27 +0300

    StringCalculator TDD exercise.

--- src/StringCalculatorKata.Test/StringCalculatorTest.cs
+++ src/StringCalculatorKata.Test/StringCalculatorTest.cs
@@ -1,9 +1,86 @@
 using NUnit.Framework;
+using System;
 using StringCalculatorKata;
 
 namespace StringCalculatorKata.Test
 {
     public class StringCalculatorTest
     {
+        [Test]
+        public void Add_EmptyStringAsParam_ReturnsZero()
+        {
+            Assert.AreEqual(0, StringCalculator.Add(""));
+        }
+
+        [Test]
+        public void Add_StringWithSingleNumberAsParam_ReturnsTheNumber()
+        {
+            Assert.AreEqual(5, StringCalculator.Add("5"));
+        }
+
+        [Test]
+        public void Add_StringWithTwoNumbersAsParam_ReturnsTheirSum()
+        {
+            Assert.AreEqual(10, StringCalculator.Add("2,8"));
+        }
+
+        [Test]
+        public void Add_StringWithThreeNumbersAsParam_ReturnsTheirSum()
+        {
+            Assert.AreEqual(14, StringCalculator.Add("2,8,4"));
+        }
+
+        [TestCase("1,2,3", 6)]
+        [TestCase("1,0,0,0,", 1)]
+        [TestCase("7,3,8,2,5", 25)]
+        [TestCase("28, 4, 10,0,0,0,7", 49)]
+        [TestCase("1,2,3,4,5,6,7,8,9,10", 55)]
+        public void Add_StringWithDifferentAmountOfNumbers_ReturnsTheirSum(string numbers, int result)
+        {
+            Assert.AreEqual(result, StringCalculator.Add(numbers));
+        }
+
+        [Test]
+        public void Add_StringWithNewLineAsDelimiter_ReturnsTheSumOfTheNUmbers()
+        {
+            Assert.AreEqual(14, StringCalculator.Add("2\n8,4"));
+        }
+
+        [TestCase("//$\n1$2$3", 6)]
+        [TestCase("//@\n2@3@4@9", 18)]
+        [TestCase("//#\n17#28#3", 48)]
+        public void Add_SyntaxSupportingDifferentDelimiters_AcceptsTheDifferentDelimiter(string numbers, int expected)
+        {
+            Assert.AreEqual(expected, StringCalculator.Add(numbers));
+        }
+
+        [Test]
+        public void Add_StringWithNegativeNumbersAsParam_ThrowsException()
+        {
+            const string numbers = "1,2,3,-5";
+            Assert.Throws<ArgumentException>(() => StringCalculator.Add(numbers), "negatives not allowed: -5");
+        }
+
+        [TestCase("1001, 1", 1)]
+        [TestCase("1000, 1", 1001)]
+        [TestCase("999, 1", 1000)]
+        [TestCase("1,2,3,2021", 6)]
+        public void Add_StringWithNumbersLargerThan1000_ReturnsTheSumIgnoringNumbersLargerThan1000(string numbers,
+            int expected)
+        {
+            Assert.AreEqual(expected, StringCalculator.Add(numbers));
+        }
+
+        [Test]
+        public void Add_StringWithLongerDelimiter()
+        {
+            Assert.AreEqual(6, StringCalculator.Add("//[***]\n1***2***3"));
+        }
+
+        [Test]
+        public void Add_StringWirthMultipleDelimiters()
+        {
+            Assert.AreEqual(23, StringCalculator.Add("//[#][&]\n5#8&10"));
+        }
     }
 }
--- src/StringCalculatorKata/StringCalculator.cs
+++ src/StringCalculatorKata/StringCalculator.cs
@@ -1,8 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace StringCalculatorKata
 {
     public class StringCalculator
-    {        
+    {
+        public static int Add(string numbers)
+        {
+            var (delimiters, rest) = ExtractDelimitersAndNumbers(numbers);
+            
+            return rest.Split(delimiters, StringSplitOptions.None).Select(ParseOrZero).Sum();
+        }
+
+        private static (string[], string) ExtractDelimitersAndNumbers(string numbers)
+        {
+            var delimiters = new List<string>{",", "\n"};
+            
+            if (!numbers.StartsWith("//"))
+            {
+                return (delimiters.ToArray(), numbers);
+            }
+
+            var parts = numbers.Split('\n');
+
+            numbers = parts[1];
+            var candidateDelimiters = parts[0].Replace("/", "");
+
+            if (candidateDelimiters.Contains("["))
+            {
+                var matches = Regex.Matches(candidateDelimiters, "[\\[!@#$%¨&*\\]+]{1}");
+                foreach (Match match in matches)
+                {
+                    delimiters.Add(match.Value);
+                }
+            }
+            
+            delimiters.Add(candidateDelimiters);
+
+            return (delimiters.ToArray(), numbers);
+        }
+
+        private static int ParseOrZero(string candidate)
+        {
+            var failure = !int.TryParse(candidate, out var result);
+
+            if (failure || result > 1000)
+            {
+                return 0;
+            }
+
+            if (result < 0)
+            {
+                throw new ArgumentException($"negatives not allowed: {result}");
+            }
+
+            return result;
+        }
     }
 }
